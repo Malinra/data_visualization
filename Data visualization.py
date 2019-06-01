8cef52040f8cdebb35f1d874b9676530da53a959
Mafeifan <247608671@qq.com>
2019-06-01 09:29:59 +0800

    first operation

--- Data visualization.py
+++ Data visualization.py
@@ -5,3 +5,11 @@ cm = np.corrcoef(df_train[cols].values.T)
 sns.set(font_scale=1.25)
 hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
 plt.show()
+
+
+
+#scatterplot
+sns.set()
+cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
+sns.pairplot(df_train[cols], size = 2.5)
+plt.show();
