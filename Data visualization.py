651cbaed46d2dad73b7c63bdfc5bd31078957907
Mafeifan <247608671@qq.com>
2019-06-01 09:27:15 +0800

    Initial commit

---
+++ Data visualization.py
@@ -0,0 +1,7 @@
+#saleprice correlation matrix
+k = 10 #number of variables for heatmap
+cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
+cm = np.corrcoef(df_train[cols].values.T)
+sns.set(font_scale=1.25)
+hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
+plt.show()
